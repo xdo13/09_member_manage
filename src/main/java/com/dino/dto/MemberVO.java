8266f584c411e9a525cfcdd511e6d07bdacfebe6
xdo13 <xdo13@naver.com>
2024-11-21 16:54:03 +0900

    MemberVO 코드 작성

--- src/main/java/com/dino/dto/MemberVO.java
+++ src/main/java/com/dino/dto/MemberVO.java
@@ -1,5 +1,16 @@
 package com.dino.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter  @Setter @ToString
 public class MemberVO {
+	private String username;
+	private String userid;
+	private String pwd;
+	private String email;
+	private String phone;
+	private int admin;
 
 }
