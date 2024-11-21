2a60c57413ad61686e0a7f156c9e72bf8ff9f34f
xdo13 <xdo13@naver.com>
2024-11-21 17:55:35 +0900

    MemberDAO에 회원 인증을 위한 메서드 추가

--- src/main/java/com/dino/dao/MemberDAO.java
+++ src/main/java/com/dino/dao/MemberDAO.java
@@ -1,10 +1,14 @@
 package com.dino.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
+
+import com.dino.dto.MemberVO;
 
 public class MemberDAO {
 		public MemberDAO() {
@@ -28,6 +32,82 @@ public class MemberDAO {
 		DataSource ds = (DataSource) envContext.lookup("jdbc/mysql");
 		Connection conn = ds.getConnection();
 		return conn;
+	}
+	//사용자 인증 메서드
+	public int userCheck(String userid, String pwd) {
+		int result = -1;
+		String sql = "select pwd from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString("pwd") != null && rs.getString("pwd").equals(pwd)) {
+					result = 1;
+				}else {
+					result = 0;
+				}
+			}else {
+				result = -1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if(conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	// 아이디로 회원 정보 가져오는 메서드
+	public MemberVO getMember(String userid) {
+		MemberVO mVo = null;
+		String sql = "select * from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				mVo = new MemberVO();
+				mVo.setUsername(rs.getString("username"));
+				mVo.setUserid(rs.getString("userid"));
+				mVo.setPwd(rs.getString("pwd"));
+				mVo.setEmail(rs.getString("email"));
+				mVo.setPhone(rs.getString("phone"));
+				mVo.setAdmin(rs.getInt("admin"));
+			
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if(conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return mVo;
 	}
 
 }
