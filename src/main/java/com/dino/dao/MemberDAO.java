d0af6039a23ec5a64647e8324ef7f257e21b62bf
xdo13 <xdo13@naver.com>
2024-11-21 17:01:08 +0900

    MemberDao 커넥션풀 연결 코드 작성

--- src/main/java/com/dino/dao/MemberDAO.java
+++ src/main/java/com/dino/dao/MemberDAO.java
@@ -1,5 +1,33 @@
 package com.dino.dao;
 
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 public class MemberDAO {
+		public MemberDAO() {
+	}
+	
+		private static MemberDAO instance = new MemberDAO();
+		
+		
+		public static MemberDAO getInstance() {
+				return instance;
+		}
+		
+		Connection Conn =null;
+		
+		//커넥션 풀
+		
+	public Connection getConnection() throws Exception{
+		
+		Context initContext = new InitialContext();
+		Context envContext = (Context) initContext.lookup("java:/comp/env");
+		DataSource ds = (DataSource) envContext.lookup("jdbc/mysql");
+		Connection conn = ds.getConnection();
+		return conn;
+	}
 
 }
