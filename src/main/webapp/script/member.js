7bb7991f1b038d5a355e01bd2a17dc969132ae54
xdo13 <xdo13@naver.com>
2024-11-22 16:35:57 +0900

    회원가입 입력폼 추가 및 자바 스크립트 유효성 검사 추가

--- src/main/webapp/script/member.js
+++ src/main/webapp/script/member.js
@@ -4,6 +4,8 @@ function loginCheck(){
 		frm.userid.focus();
 		return false;
 	}
+	var url = "idCheck.do?userid=" +document.frm.userid.value;
+	window.open(url, "_blank_1", "toolbar=no, menubar=no, scrollbars=yes, resizeable=no, width=450, height=200");
 	
 	if(document.frm.pwd.value == "") {
 		alert("암호는 반드시 입력해야 합니다");
