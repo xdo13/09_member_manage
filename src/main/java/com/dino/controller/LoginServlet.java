2844c3e3876a0c02a2f60ce33431b111200b0a3a
xdo13 <xdo13@naver.com>
2024-11-22 15:55:53 +0900

    dopost에 메서드추가(유효성검사)

--- src/main/java/com/dino/controller/LoginServlet.java
+++ src/main/java/com/dino/controller/LoginServlet.java
@@ -6,7 +6,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
 import java.io.IOException;
+
+import com.dino.dao.MemberDAO;
+import com.dino.dto.MemberVO;
 
 @WebServlet("/login.do")
 public class LoginServlet extends HttpServlet {
@@ -19,8 +24,23 @@ public class LoginServlet extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		doGet(request, response);
+		String url = "member/login.jsp"; //url 변수에 login.jsp 페이지를 담는다.
+		String userid = request.getParameter("userid");
+		String pwd = request.getParameter("pwd");
+		MemberDAO mDao = MemberDAO.getInstance();
+		int result = mDao.userCheck(userid, pwd);
+		if(result == 1) {
+			MemberVO mVo = mDao.getMember(userid);
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", mVo);
+			request.setAttribute("message", "회원가입에 성공했습니다.");
+			url = "main.jsp";
+		}else if (result == 0) {
+			request.setAttribute("message", "비밀번호가 맞지 않습니다");
+		}else if (result == -1) {
+			request.setAttribute("message", "존재하지 않는 회원입니다.");
+		}
+		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
+		dispatcher.forward(request, response);
 	}
-
 }
